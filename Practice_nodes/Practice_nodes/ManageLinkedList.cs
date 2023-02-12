1b8ef1e97845698c10cecc9a14b0ad69e0faa521
Toby Stupp <40300257+tobyStupp@users.noreply.github.com>
2023-02-12 02:30:09 +0200

    Add files via upload
    Starting a new project to practice

---
+++ Practice_nodes/Practice_nodes/ManageLinkedList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unit4;
+
+namespace Practice_nodes
+{
+    class ManageLinkedList
+    {
+        public static Node<int> GenerateRandomLinkedList(int size = 10)
+        {
+            // generates and returns a random List
+            throw new NotImplementedException();
+        }
+        public static void PrintLinkedList (Node<int> p)
+        {
+            if (p.GetNext()==null)
+                Console.WriteLine(p.GetValue());
+            else
+            {
+                Console.Write(p.GetValue().ToString() + ", ");
+                PrintLinkedList(p.GetNext());
+            }
+        }
+
+    }
+}
