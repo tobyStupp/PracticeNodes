c57226040c296e115ea5851402801e3d60bec0d1
Toby Stupp <40300257+tobyStupp@users.noreply.github.com>
2023-02-19 02:23:26 +0200

    Add files via upload

--- Practice_nodes/Practice_nodes/Program.cs
+++ Practice_nodes/Practice_nodes/Program.cs
@@ -9,24 +9,26 @@ namespace Practice_nodes
 {
     class Program
     {
-        public Node<int> InsertSorted (Node<int> head, Node<int> last)
+        public static Node<int> Sort(Node<int> head)
         {
             //takes the Node after last Node and inserts it in its sorted position
             // assert that all nodes between head and last are sorted
             // the method returns the first Node (which might change) of the sorted Linked List
+            // throw new NotImplementedException();
             throw new NotImplementedException();
 
         }
-        public static  Node<int> Sort (Node<int> head)
-        {
-            Node<int> last = head;
-            while (last.GetNext()!=null)
-            {
-                head = InsertSorted(head, last);
-                last = last.GetNext();
-            }
-            return head;
-        }
+        //public static  Node<int> Sort (Node<int> head)
+        //{
+        //    Node<int> dummy = new Node<int>(int.MinValue, head);
+        //    Node<int> last = head;
+        //    while (last!=null)
+        //    {
+        //        last = InsertSorted(dummy, last);
+               
+        //    }
+        //    return dummy.GetNext();
+        //}
         public static void Join (Node<int> a, Node <int> b)
         {
             // adds the linked list starting at b at the end of the link list starting from a
@@ -49,19 +51,21 @@ namespace Practice_nodes
         static void Main(string[] args)
         {
             //don't forget to implement the Generated Random List 
-            Node<int> list = ManageLinkedList.GenerateRandomLinkedList(12);
+            Node<int> list = ManageLinkedList.GenerateRandomLinkedList(5);
             ManageLinkedList.PrintLinkedList(list);
+            /*
             Node<int> list1 = ManageLinkedList.GenerateRandomLinkedList(5);
             ManageLinkedList.PrintLinkedList(list1);
             Join(list, list1);
             ManageLinkedList.PrintLinkedList(list);
+            */
             list = Sort(list);
             ManageLinkedList.PrintLinkedList(list);
             //make sure that  list starts with uneven value
             //========================================
-            Node<int> even = ReturnEven( list);
-            ManageLinkedList.PrintLinkedList(even);
-
+            //Node<int> even = ReturnEven( list);
+            //ManageLinkedList.PrintLinkedList(even);
+            Console.ReadKey();
 
         }
     }
